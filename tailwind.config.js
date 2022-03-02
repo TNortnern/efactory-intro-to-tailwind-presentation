0a471318e5cc476a54bbec42820316d3d8954181
tnorthern <traynorthern@yahoo.com>
2022-03-02 07:55:42 -0600

    Initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        proxima: ["proxima-nova", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      padding: {
+        'mobile-section-padding': '41px',
+      }
+    },
+  },
+};
